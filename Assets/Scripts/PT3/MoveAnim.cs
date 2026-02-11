87f7d5922371a4cb86c58b7480c036fc3bd7171b
CIITSHSDTP <ciitshsdtp30@ciit.edu.ph>
2026-02-11 14:17:41 +0800

    Initial commit

--- Assets/Scripts/PT3/MoveAnim.cs
+++ Assets/Scripts/PT3/MoveAnim.cs
@@ -1,16 +1,68 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MoveAnim : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] float rotationFactorPerFrame;
+
+    PlayerInputSystem inputSystem;
+    CharacterController controller;
+    Animator animator;
+
+    Vector3 currentInputMovement;
+    bool isMovementPressed;
+
+    private void Awake()
     {
-        
+        inputSystem = new PlayerInputSystem();
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+
+        inputSystem.PlayerMovement.Move.performed += OnMovementInputs;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable() => inputSystem.PlayerMovement.Enable();
+
+    private void OnDisable() => inputSystem.PlayerMovement.Disable();
+
+
+    private void Update()
     {
-        
+        HandleRotation();
+        HandleAnimation();
+        controller.Move(currentInputMovement * Time.deltaTime);
+    }
+
+    private void OnMovementInputs(InputAction.CallbackContext context) 
+    { 
+        Vector2 inputMovement = context.ReadValue<Vector2>();
+        currentInputMovement.x = inputMovement.x;
+        currentInputMovement.z = inputMovement.y;
+        isMovementPressed = inputMovement.x != 0 || inputMovement.y != 0;
+    }
+
+    private void HandleAnimation() 
+    {
+        if (isMovementPressed)
+            animator.SetBool("isWalking", true);
+        else
+            animator.SetBool("isWalking", false);
+    }
+
+    private void HandleRotation() 
+    {
+        Vector3 positionToLookAt;
+        positionToLookAt.x = currentInputMovement.x;
+        positionToLookAt.y = 0f;
+        positionToLookAt.z = currentInputMovement.z;
+
+        Quaternion currentRotation = transform.rotation;
+
+        if (isMovementPressed) 
+        { 
+            Quaternion targetRotation = Quaternion.LookRotation(positionToLookAt);
+            transform.rotation = Quaternion.Slerp(currentRotation, targetRotation, rotationFactorPerFrame * Time.deltaTime);
+        }
     }
 }
